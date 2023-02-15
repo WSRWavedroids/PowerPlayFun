c68167f55ccf586c47e5c85783479359b8548af3
WSRWavedroids <wavedroid13206@gmail.com>
2023-02-14 20:30:00 -0600

    Actually finished a RoadRunner program lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueA2RR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueA2RR.java
@@ -4,11 +4,13 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.Robot;
 
 @Config
 @Autonomous(name = "Blue A2 RoadRunner", group = "drive")
@@ -19,6 +21,7 @@ public class BlueA2RR extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Robot robot = new Robot();
 
         //Starting position is -36, 63
         Pose2d startPose = new Pose2d(-36, 63, Math.toRadians(0));
@@ -26,35 +29,71 @@ public class BlueA2RR extends LinearOpMode {
         drive.setPoseEstimate(startPose);
 
         Trajectory toFirstPole = drive.trajectoryBuilder(new Pose2d())
-                .forward(27)
+                //Move forward 27 inches (-36, 63) -> (-36, 36)
+                //Move left 36 inches (-36, 36) -> (0, 36)
+                .splineTo(new Vector2d(36, -27), Math.toRadians(0))
+                //Arm witchery *DISPLACEMENT MARKER HERE*
+                .addDisplacementMarker(() -> {
+                    robot.moveArm("Up");
+                })
+                //Move forward 2 inches (0, 36) -> (0, 34)
+                .splineTo(new Vector2d(0, -2), Math.toRadians(0))
+                .build();
+
+        Trajectory toConeStackInitial = drive.trajectoryBuilder(startPose)
+                .addDisplacementMarker(() -> {
+                    robot.moveArm("Down");
+                    robot.openAndCloseClaw(0.3);
+                })
+                //Move backward 2 inches (0, 34) -> (0, 36)
+                .splineTo(new Vector2d(0, 2), Math.toRadians(0))
+                //Move right 12 inches (0, 36) -> (-12, 36)
+                //Move forward 24 inches (-12, 36) -> (-12, 12)
+                //Turn 90 degrees to the right *ADJUST HEADING*
+                .splineToSplineHeading(new Pose2d(-12, -24, Math.toRadians(0)), Math.toRadians(-90))
+                //Move forward 40(?) inches (-12, 12) -> (-52, 12)
+                .splineTo(new Vector2d(-40, 0), Math.toRadians(0))
+                //Pick up a cone *DISPLACEMENT MARKER HERE*
+                .addDisplacementMarker(() -> {
+                    robot.openAndCloseClaw(0);
+                    robot.moveArm("Up");
+                })
+                .build();
+
+        Trajectory cycleCone = drive.trajectoryBuilder(startPose)
+                //1. Move backward 28 inches (-52, 12) -> (-24, 12)
+                //2. Turn 90 degrees to left *ADJUST HEADING*
+                .splineToSplineHeading(new Pose2d(28, 0, Math.toRadians(-90)), Math.toRadians(0))
+                //3. Put cone on high pole *DISPLACEMENT MARKER HERE*
+                .addDisplacementMarker(() -> {
+                    robot.moveArm("Down");
+                    robot.openAndCloseClaw(0.3);
+                })
+                //4. Turn 90 degrees to right *ADJUST HEADING*
+                //5. Move forward 28 inches (-24, 12) -> (-52, 12)
+                .splineToSplineHeading(new Pose2d(-28, 0, Math.toRadians(0)), Math.toRadians(-90))
+                //6. Pick up a cone *DISPLACEMENT MARKER HERE*
+                .addDisplacementMarker(() -> {
+                    robot.openAndCloseClaw(0);
+                    robot.moveArm("Up");
+                })
+
                 .build();
 
         waitForStart();
         //Coordinate reference: x-axis is left and right between auto cone stacks, y-axis is up and down between substations
         //From this position... forward is subtracting from y, backward is adding to y, left is adding to x, right is subtracting from x
 
-        //Move forward 27 inches (-36, 63) -> (-36, 36)
-        //Move left 36 inches (-36, 36) -> (0, 36)
-        //Move forward 2 inches (0, 36) -> (0, 34)
-        //Arm witchery *DISPLACEMENT MARKER HERE*
-        //Move backward 2 inches (0, 34) -> (0, 36)
-        //Move right 12 inches (0, 36) -> (-12, 36)
-        //Move forward 24 inches (-12, 36) -> (-12, 12)
-        //Turn 90 degrees to the right *ADJUST HEADING*
-        //Move forward 40(?) inches (-12, 12) -> (-52, 12)
-        //Pick up a cone *DISPLACEMENT MARKER HERE*
-        //1. Move backward 28 inches (-52, 12) -> (-24, 12)
-        //2. Turn 90 degrees to left *ADJUST HEADING*
-        //3. Put cone on high pole *DISPLACEMENT MARKER HERE*
-        //4. Turn 90 degrees to right *ADJUST HEADING*
-        //5. Move forward 28 inches (-24, 12) -> (-52, 12)
-        //6. Pick up a cone *DISPLACEMENT MARKER HERE*
-        //Repeat steps 1-6 as much as possible
-        //Get to AprilTags position (figure out where we will be at that point)
+
 
         if (isStopRequested()) return;
 
+        robot.openAndCloseClaw(0);
         drive.followTrajectory(toFirstPole);
+        drive.followTrajectory(toConeStackInitial);
+        drive.followTrajectory(cycleCone);
+        //Repeat cycleCone trajectory as much as possible
+        //Get to AprilTags position (figure out where we will be at that point)
 
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("finalX", poseEstimate.getX());
